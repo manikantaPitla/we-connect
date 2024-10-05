b45bace5e320f4688f0589880dd95b8f0036e365
maniDynamic <pitlamanikanta8185@gmail.com>
2024-10-05 13:44:31 +0530

    Basic code

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, signOut } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzx9xLFbbuHgpZnEbQx_NpvzFnB4EmlM8",
-  authDomain: "we-connect-dev-official.firebaseapp.com",
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
   projectId: "we-connect-dev-official",
   storageBucket: "we-connect-dev-official.appspot.com",
-  messagingSenderId: "710292818478",
-  appId: "1:710292818478:web:32c2d51f18efb03f572dbf",
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
   measurementId: "G-GG1MB1YK1W",
 };
 
